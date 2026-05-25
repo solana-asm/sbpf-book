e4cf46eded532d0407ac0fcbc74ed76db672b5ee
prianshpate18 <priyansh.180804@gmail.com>
2026-05-25 20:41:12 +0530

    fix: GA via @next/third-parties

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,29 +1,12 @@
-import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const GA_ID = 'G-BXLLDK3M84';
 
 export function Analytics() {
-  // Skip analytics in dev so localhost traffic doesn't pollute the production property.
+  // Skip in dev so localhost traffic doesn't pollute the production property.
   if (process.env.NODE_ENV !== 'production') {
     return null;
   }
 
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="ga-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-    </>
-  );
+  return <GoogleAnalytics gaId={GA_ID} />;
 }
